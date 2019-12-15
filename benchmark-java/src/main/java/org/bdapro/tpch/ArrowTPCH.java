fa6951d2dc4436b9d3c6274f4cee8fd15fd65de1
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2019-12-15 17:26:54 +0100

    Exclude file-to-memory loading

--- benchmark-java/src/main/java/org/bdapro/tpch/ArrowTPCH.java
+++ benchmark-java/src/main/java/org/bdapro/tpch/ArrowTPCH.java
@@ -5,43 +5,57 @@ import org.apache.arrow.vector.*;
 import org.apache.arrow.vector.ipc.ArrowFileReader;
 import org.apache.arrow.vector.ipc.SeekableReadChannel;
 import org.apache.arrow.vector.ipc.message.ArrowBlock;
-import org.apache.arrow.vector.types.Types;
 import org.apache.arrow.vector.types.pojo.Schema;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-@State(Scope.Benchmark)
+
 public class ArrowTPCH {
-    public static double executeQuerySix(String lineItemFilePath) throws IOException {
-        File arrowFile = new File(lineItemFilePath);;
-        FileInputStream fileInputStream = new FileInputStream(arrowFile);
-        ArrowFileReader arrowFileReader = new ArrowFileReader(new SeekableReadChannel(fileInputStream.getChannel()),
-                new RootAllocator(Integer.MAX_VALUE));
-        VectorSchemaRoot root = arrowFileReader.getVectorSchemaRoot();
-        Schema schema = root.getSchema();
 
-        List<ArrowBlock> arrowBlocks = arrowFileReader.getRecordBlocks();
+    @State(Scope.Benchmark)
+    public static class MySate{
+        private List<ArrowBlock> arrowBlocks;
+        private ArrowFileReader arrowFileReader;
+        private VectorSchemaRoot root;
 
+        @Param({"lineitem10.arrow","lineitem30.arrow","lineitem100.arrow","lineitem300.arrow","lineitem1k.arrow","lineitem3k.arrow","lineitem10k.arrow","lineitem30k.arrow"})
+        public String lineItemFilePath;
+
+        @Setup(Level.Trial)
+        public void setup() throws IOException {
+            File arrowFile = new File(lineItemFilePath);
+            FileInputStream fileInputStream = new FileInputStream(arrowFile);
+            arrowFileReader = new ArrowFileReader(new SeekableReadChannel(fileInputStream.getChannel()),
+                    new RootAllocator(Integer.MAX_VALUE));
+            root = arrowFileReader.getVectorSchemaRoot();
+            Schema schema = root.getSchema();
+
+            arrowBlocks = arrowFileReader.getRecordBlocks();
+        }
+
+        @TearDown(Level.Trial)
+        public void doTearDown() throws IOException {
+            arrowFileReader.close();
+        }
+    }
+
+    @Benchmark @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public double executeQuerySix(MySate state) throws IOException {
         double totalRevenue = 0.0;
-        for (ArrowBlock arrowBlock : arrowBlocks) {
-            if (!arrowFileReader.loadRecordBatch(arrowBlock)) { // load the batch
+        for (ArrowBlock arrowBlock : state.arrowBlocks) {
+            if (!state.arrowFileReader.loadRecordBatch(arrowBlock)) { // load the batch
                 throw new IOException("Expected to read record batch");
             }
             // Get columns based on column order on TPC-H LineItem table
-            BigIntVector lQuantity = (BigIntVector) root.getVector(4);
-            Float8Vector lExtendedPrice = (Float8Vector) root.getVector(5);
-            Float8Vector lDiscount = (Float8Vector) root.getVector(6);
-            TimeStampSecVector lShipDate = (TimeStampSecVector) root.getVector(10);
+            BigIntVector lQuantity = (BigIntVector) state.root.getVector(4);
+            Float8Vector lExtendedPrice = (Float8Vector) state.root.getVector(5);
+            Float8Vector lDiscount = (Float8Vector) state.root.getVector(6);
+            TimeStampSecVector lShipDate = (TimeStampSecVector) state.root.getVector(10);
 
             //Todo: Check if lQuantity, lPrice, lDiscount, and lShidate have the same size
             double currentBlockRevenue = 0.0;
@@ -56,7 +70,6 @@ public class ArrowTPCH {
             }
             totalRevenue += currentBlockRevenue;
         }
-        arrowFileReader.close();
         return totalRevenue;
     }
 }
